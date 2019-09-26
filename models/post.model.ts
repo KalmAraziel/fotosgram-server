d08e346e62fe856c12780f70f501b8463b7317f2
Marcos Ahumada <marcos.ahumada@example.com>
2019-09-26 18:02:51 -0300

    modelo Post

---
+++ models/post.model.ts
@@ -0,0 +1,40 @@
+import { Schema, model, Document } from "mongoose";
+import { Usuario } from './usuario.model';
+
+const postSchena = new Schema({
+    created: {
+        type: Date
+    },
+    mensajes: {
+        type: String
+    },
+    img: [{
+        type: String
+    }],
+    coords: {
+        type: String
+    },
+    usuario: {
+        type: Schema.Types.ObjectId,
+        ref: 'Usuario',
+        required: [ true, 'Usuario debe existir' ]
+    }
+
+});
+
+// triger se guarda y se actualiza la fecha
+postSchena.pre<IPost>('save', function( next) {
+    this.created = new Date();
+    next();
+});
+
+interface IPost extends Document {
+    created: Date;
+    mensajes: string;
+    img: string[];
+    coords: string;
+    usuario: string;
+
+}
+
+export const Post = model<IPost>('Post', postSchena);
