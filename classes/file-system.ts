ec0dad4e3a4b64e26d7f85c85f5c612f0c1ee61b
Marcos Ahumada <marcos.ahumada@example.com>
2019-09-27 15:28:48 -0300

    nombre unico archivo

--- classes/file-system.ts
+++ classes/file-system.ts
@@ -1,13 +1,23 @@
 import { FileUpload } from '../interfaces/file-upload';
 import path from 'path';
 import fs from 'fs';
+import uniqid from 'uniqid';
 export default class  FileSystem {
     constructor() {}
 
-    guardarImagenTmp(imagen: FileUpload, userId: string) {
+    guardarImagenTmp(archivo: FileUpload, userId: string) {
+        // crear carpeta
         const path = this.crearCarpetaUsuario(userId);
+        // nombre archivo
+        const nombreArchivo = this.generarNombreUnico(archivo.name);
     }
 
+    private generarNombreUnico(nombreOriginal: string) {
+        const nombreArr = nombreOriginal.split('.');
+        const extension = nombreArr[ nombreArr.length - 1 ];
+        const idUnico = uniqid();
+        return idUnico + '.' + extension;
+    }
     crearCarpetaUsuario(userId: string) {
         const pathUser = path.resolve(__dirname, '../uploads/', userId);
         const pathUserTemp = pathUser + '/temp';
